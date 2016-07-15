7e09e64df3933798f4072239728c31a6433b093a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-07-15 11:36:27 +0900

    organize method name

--- lib/active_job/cancel.rb
+++ lib/active_job/cancel.rb
@@ -10,8 +10,8 @@ module ActiveJob
     SUPPORTED_ADAPTERS = %w(Sidekiq).freeze
 
     def cancel
-      if self.class.supported_adapter?
-        self.class.adapter_class.new.cancel(job_id, queue_name)
+      if self.class.can_cancel?
+        self.class.cancel_adapter_class.new.cancel(job_id, queue_name)
       else
         raise NotImplementedError, 'This queueing backend does not support cancel.'
       end
@@ -19,36 +19,37 @@ module ActiveJob
 
     module ClassMethods
       def cancel(job_id)
-        if supported_adapter?
-          adapter_class.new.cancel(job_id, self.queue_name)
+        if can_cancel?
+          cancel_adapter_class.new.cancel(job_id, self.queue_name)
         else
           raise NotImplementedError, 'This queueing backend does not support cancel.'
         end
       end
 
       def cancel_by(opts)
-        if supported_adapter?
-          adapter_class.new.cancel_by(opts, self.queue_name)
+        if can_cancel?
+          cancel_adapter_class.new.cancel_by(opts, self.queue_name)
         else
           raise NotImplementedError, 'This queueing backend does not support cancel_by.'
         end
       end
 
-      def supported_adapter?
-        SUPPORTED_ADAPTERS.include?(adapter)
+      def can_cancel?
+        SUPPORTED_ADAPTERS.include?(adapter_name)
       end
 
-      def adapter
-        if ActiveJob.version > Gem::Version.new('4.3')
-          self.queue_adapter.class.name.demodulize.chomp('Adapter')
-        else
-          self.queue_adapter.name.demodulize.chomp('Adapter')
+      def cancel_adapter_class
+        Object.const_get("ActiveJob::Cancel::QueueAdapters::#{adapter_name}Adapter")
+      end
+
+      private
+        def adapter_name
+          if ActiveJob.version > Gem::Version.new('4.3')
+            self.queue_adapter.class.name.demodulize.chomp('Adapter')
+          else
+            self.queue_adapter.name.demodulize.chomp('Adapter')
+          end
         end
-      end
-
-      def adapter_class
-        Object.const_get("ActiveJob::Cancel::QueueAdapters::#{adapter}Adapter")
-      end
     end
   end
 end
