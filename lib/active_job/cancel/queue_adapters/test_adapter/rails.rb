743f1e1d7b4c722822d54cc7885264aa540b514d
Brian Moran <brian@trucentive.com>
2021-04-21 15:22:07 -0700

    make sure names and type match up with what other parts of the ecosystem expect

--- lib/active_job/cancel/queue_adapters/test_adapter/rails.rb
+++ lib/active_job/cancel/queue_adapters/test_adapter/rails.rb
@@ -2,13 +2,15 @@ module ActiveJob
   module QueueAdapters
     class TestAdapter
       def job_to_hash(job, extras = {})
-        {
+        # These values should match up with what other ActiveJob::QueueAdapters publish. At minimum, job_class, job_id...
+        # Otherwise, ActiveJob test helpers like assert_enqueued_with will break.
+        HashWithIndifferentAccess.new({
           job_id: job.job_id,
           job: job.class,
           job_class: job.class.to_s,
           args: job.serialize.fetch('arguments'),
           queue: job.queue_name
-        }.merge!(extras)
+        }.merge!(extras))
       end
     end
   end
