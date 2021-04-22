37957c4d22c4b572bbed3e95c0820226f1f99b2f
Brian Moran <brian@trucentive.com>
2021-04-21 20:49:11 -0700

    need arguments, may need queue_name

--- lib/active_job/cancel/queue_adapters/test_adapter/rails.rb
+++ lib/active_job/cancel/queue_adapters/test_adapter/rails.rb
@@ -1,8 +1,9 @@
 module ActiveJob
   module QueueAdapters
     class TestAdapter
+
       def job_to_hash(job, extras = {})
-        # These values should match up with what other ActiveJob::QueueAdapters publish. At minimum, job_class, job_id...
+        # These values should match up with what other ActiveJob::QueueAdapters publish. At minimum, job_class, job_id, arguments...
         # Otherwise, ActiveJob test helpers like assert_enqueued_with will break.
         HashWithIndifferentAccess.new({
           id: job.job_id,
@@ -10,7 +11,9 @@ module ActiveJob
           job: job.class,
           job_class: job.class.to_s,
           args: job.serialize.fetch('arguments'),
-          queue: job.queue_name
+          arguments: job.serialize.fetch('arguments'),
+          queue: job.queue_name,
+          queue_name: job.queue_name
         }.merge!(extras))
       end
     end
