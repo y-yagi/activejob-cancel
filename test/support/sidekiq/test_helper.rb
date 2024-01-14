d34b063cf75a5b7ef09ae2dc406e087c22722d84
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-01-14 11:31:44 +0900

    Support Sidekiq 7.0+

--- test/support/sidekiq/test_helper.rb
+++ test/support/sidekiq/test_helper.rb
@@ -7,13 +7,28 @@ Sidekiq.configure_server do |config|
 end
 
 def execute_with_launcher
-  sidekiq = Sidekiq::Launcher.new({queues: [FailJob.queue_name.call],
-                                   environment: "test",
-                                   concurrency: 1,
-                                   timeout: 1,
-                                  })
-  Sidekiq.average_scheduled_poll_interval = 3
-  Sidekiq.options[:poll_interval_average] = 1
+  if Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new("7")
+    config = Sidekiq.default_configuration
+    config.queues = [FailJob.queue_name.call]
+    config.concurrency = 1
+    config.average_scheduled_poll_interval = 0.5
+    config.merge!(
+      environment: "test",
+      timeout: 1,
+      poll_interval_average: 3
+    )
+  else
+    config = {
+      queues: [FailJob.queue_name.call],
+      environment: "test",
+      concurrency: 1,
+      timeout: 1,
+      average_scheduled_poll_interval: 0.5,
+      poll_interval_average: 3
+    }
+  end
+
+  sidekiq = Sidekiq::Launcher.new(config)
   sidekiq.run
   yield
   sidekiq.stop
